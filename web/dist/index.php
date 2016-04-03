51bf7ff8a8580699b3e8469fb1b8fc1ff0c8f7dd
Yuriy Yevstihnyeyev <lider961@gmail.com>
2016-04-03 22:36:58 +0300

    Fixing errors part 3

--- web/dist/index.php
+++ web/dist/index.php
@@ -3,13 +3,14 @@
 	{
 		$conn = new mysqli('localhost', 'root', 'matti', 'mamk_acs');
 		$query = "SELECT DISTINCT special_key FROM SPMas_users WHERE username='".($_POST['username'])."' and password='".($_POST['password'])."' LIMIT 1";
+		echo $query;
 		$special_key = $conn->query($query);
 		if ($special_key->num_rows > 0)
 		{
 			while($special_key_obj = $special_key->fetch_assoc()) 
 			{
 				session_start();
-				$_SESSION['user_key']=$special_key_obj['id'];
+				$_SESSION['user_key']=$special_key_obj['special_key'];
 				header('page.php');
 			}
 		}
