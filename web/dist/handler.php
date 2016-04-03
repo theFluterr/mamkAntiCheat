6bf3a2090fc3aba9196f1ab7919bedfec39ba65a
Yuriy Yevstihnyeyev <lider961@gmail.com>
2016-04-04 00:48:29 +0300

    no message

--- web/dist/handler.php
+++ web/dist/handler.php
@@ -24,7 +24,7 @@ if(isset($_POST['check_adminKEY102ao']))
 			while($answer_obj = $answer->fetch_assoc()) 
 			{
 				$time = gmdate("Y-m-d H:i:s", $answer_obj['unix_time']);
-				$answer=$answer_obj['answer_value'];
+				$answeropt=$answer_obj['answer_value'];
 			}
 		}
 		$query = "SELECT DISTINCT * FROM test_log_answers WHERE user_id='1' and question_id='2' ORDER by unix_time DESC LIMIT 1";
@@ -37,8 +37,8 @@ if(isset($_POST['check_adminKEY102ao']))
 				$answer2=$answer_obj['answer_value'];
 			}
 		}
-		similar_text($answer,$answer2,$percentage);
-		echo '<p>'.($answer).'<br><b>'.($time).'</b><br><b>Checked closest student answers: <b>'.($percentage).'%</b></b></p>';
+		similar_text($answeropt,$answer2,$percentage);
+		echo '<p>'.($answeropt).'<br><b>'.($time).'</b><br><b>Checked closest student answers: <b>'.($percentage).'%</b></b></p>';
 	}
 	if($_POST['check_adminKEY102ao']=='3') 
 	{
@@ -62,7 +62,7 @@ if(isset($_POST['check_adminKEY102ao']))
 			while($answer_obj = $answer->fetch_assoc()) 
 			{
 				$time = gmdate("Y-m-d H:i:s", $answer_obj['unix_time']);
-				$answer=$answer_obj['answer_value'];
+				$answeropt=$answer_obj['answer_value'];
 			}
 		}
 		$query = "SELECT DISTINCT * FROM test_log_answers WHERE user_id='1' and question_id='2' ORDER by unix_time DESC LIMIT 1";
@@ -75,7 +75,7 @@ if(isset($_POST['check_adminKEY102ao']))
 				$answer2=$answer_obj['answer_value'];
 			}
 		}
-		similar_text($answer,$answer2,$percentage);
+		similar_text($answeropt,$answer2,$percentage);
 		echo '<p>'.($answer2).'<br><b>'.($time2).'</b><br><b>Checked closest student answers: <b>'.($percentage).'%</b></b></p>';
 	}
 }
