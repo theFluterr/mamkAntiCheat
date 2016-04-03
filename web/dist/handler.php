5df33849fa806a9472bdd69bbb699f51a96b006f
Yuriy Yevstihnyeyev <lider961@gmail.com>
2016-04-04 00:04:34 +0300

    no message

--- web/dist/handler.php
+++ web/dist/handler.php
@@ -2,7 +2,7 @@
 if(isset($_POST['check_adminKEY102ao'])) 
 {
 	
-	if($_POST['check_adminKEY102ao']==2) 
+	if($_POST['check_adminKEY102ao']=='2') 
 	{
 		$conn = new mysqli('localhost', 'root', 'matti', 'mamk_acs');
 		$query = "SELECT DISTINCT * FROM test_log_answers WHERE user_id='2' and question_id='1' ORDER by unix_time DESC LIMIT 1";
@@ -15,7 +15,7 @@ if(isset($_POST['check_adminKEY102ao']))
 			}
 		}
 	}
-	elseif($_POST['check_adminKEY102ao']==1) 
+	elseif($_POST['check_adminKEY102ao']=='1') 
 	{
 		$conn = new mysqli('localhost', 'root', 'matti', 'mamk_acs');
 		$query = "SELECT DISTINCT * FROM test_log_answers WHERE user_id='2' and question_id='2' ORDER by unix_time DESC LIMIT 1";
